c6464e1e3c2a17691a9f679c0e7a75859c70aa2e
Pratik Gorade <pratikgorade0@gmail.com>
2021-02-02 22:31:02 +0530

    duplicate user validation for admin_signup
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- app/server/routes/admin.py
+++ app/server/routes/admin.py
@@ -1,4 +1,4 @@
-from fastapi import Body, APIRouter
+from fastapi import Body, APIRouter, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.security import HTTPBasicCredentials
 from passlib.context import CryptContext
@@ -7,6 +7,7 @@ from app.server.auth.admin import validate_login
 from app.server.auth.jwt_handler import signJWT
 from app.server.database.database import add_admin
 from app.server.models.admin import AdminModel
+from app.server.database.database import admin_collection
 
 router = APIRouter()
 
@@ -23,7 +24,16 @@ async def admin_login(admin: HTTPBasicCredentials = Body(...)):
 
 @router.post("/")
 async def admin_signup(admin: AdminModel = Body(...)):
-    # TODO: Perform validation to check if user exists.
-    admin.password = hash_helper.encrypt(admin.password)
-    new_admin = await add_admin(jsonable_encoder(admin))
-    return new_admin
+    # retrive the first record that matches with the email in request
+    # if no records found, create new user
+    # otherwise raise 409 error
+    old_admin = await admin_collection.find_one({'email': admin.email})
+    if old_admin == None:
+        admin.password = hash_helper.encrypt(admin.password)
+        new_admin = await add_admin(jsonable_encoder(admin))
+        return new_admin
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User with this email already exists"
+        )
