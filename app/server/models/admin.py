f6be58306072d01c5950434e54e48b00a54ad566
Pratik Gorade <pratikgorade0@gmail.com>
2021-02-03 23:59:20 +0530

    added response and error response model for admin this will help when returning /admin/delete path
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- app/server/models/admin.py
+++ app/server/models/admin.py
@@ -14,3 +14,20 @@ class AdminModel(BaseModel):
                 "password": "Your password goes here."
             }
         }
+
+def ResponseModel(data, message):
+    return {
+        "data": [
+            data
+        ],
+        "code": 200,
+        "message": message,
+    }
+
+
+def ErrorResponseModel(error, code, message):
+    return {
+        "error": error,
+        "code": code,
+        "message": message
+    }
