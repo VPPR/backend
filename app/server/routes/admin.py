091d4028cb4ed29ebe2c76869cce76b597051c87
Pratik Gorade <pratikgorade0@gmail.com>
2021-02-03 22:59:56 +0530

    shifted duplicate user check for /admin/signup path from app/server/routes/admin to app/server/database/database
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- app/server/routes/admin.py
+++ app/server/routes/admin.py
@@ -27,13 +27,13 @@ async def admin_signup(admin: AdminModel = Body(...)):
     # retrive the first record that matches with the email in request
     # if no records found, create new user
     # otherwise raise 409 error
-    old_admin = await admin_collection.find_one({'email': admin.email})
-    if old_admin == None:
-        admin.password = hash_helper.encrypt(admin.password)
-        new_admin = await add_admin(jsonable_encoder(admin))
-        return new_admin
-    else:
+    admin.password = hash_helper.encrypt(admin.password)
+    new_admin = await add_admin(jsonable_encoder(admin))
+    if new_admin == None:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="User with this email already exists"
         )
+    else:
+        return new_admin
+    
